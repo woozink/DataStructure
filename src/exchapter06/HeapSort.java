580c28e15b158c7dd5ca271517e1c83ecc51b66f
woozink <k0109974@naver.com>
2022-02-08 18:15:13 +0900

    heapSort

---
+++ src/exchapter06/HeapSort.java
@@ -0,0 +1,39 @@
+package exchapter06;
+
+public class HeapSort {
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+
+    static void downHeap(int[] a, int left, int right) {
+        int temp = a[left];
+        int child;
+        int parent;
+
+        for (parent = left; parent < (right + 1) / 2; parent = child) {
+            int cl = parent * 2 + 1;
+            int cr = cl + 1;
+            child = (cr <= right && a[cr] > a[cl] ? cr : cl);
+            if (temp >= a[child]) {
+                break;
+            }
+            a[parent] = a[child];
+
+
+        }
+        a[parent] = temp;
+
+    }
+
+    static void heapSort(int[] a, int n) {
+        for (int i = (n - 1) / 2; i >= 0; i--) {
+            downHeap(a, i, n - 1);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            swap(a, 0, i);
+            downHeap(a, 0, i - 1);
+        }
+    }
+}
