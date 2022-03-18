95788ee4a2e9dfee94cb00bdbaf6d144b943fa3b
woozink <k0109974@naver.com>
2022-03-18 14:27:45 +0900

    insertionSort

---
+++ src/sortAlgorithm/InsertionSortion.java
@@ -0,0 +1,33 @@
+package sortAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class InsertionSortion {
+    public ArrayList<Integer> sort(ArrayList<Integer>dataList){
+        for(int index = 0; index < dataList.size()-1; index++){
+            for(int index2 = index+1; index2>0; index2--){
+                if(dataList.get(index2) < dataList.get(index2-1)){
+                    Collections.swap(dataList,index2,index2-1);
+                }else{
+                    break;
+                }
+            }
+        }
+        return dataList;
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> testData = new ArrayList<Integer>();
+
+        for(int i =0; i <100; i++){
+            testData.add((int)(Math.random()*100));
+        }
+
+
+        InsertionSortion iSort = new InsertionSortion();
+        System.out.println(iSort.sort(testData));
+    }
+}
